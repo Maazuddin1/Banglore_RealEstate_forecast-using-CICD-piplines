0472b84ba365f7cacbc66da7801f36394c1032ab
Maaz Uddin <143177820+Maazuddin1@users.noreply.github.com>
2024-12-27 16:13:02 +0530

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,7 +3,7 @@ import pytest
 import pickle
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from model import ModelBuilder
 
 # Helper function to load preprocessed data
